1a859c72e0a5ee851d0a356f4267876c62f486a9
Joe Potenza <joseph.potenza@gmail.com>
2026-03-05 09:32:37 -0500

    Fix incorrect value on load and add alt text for image

--- src/components/WeatherCard/WeatherCard.jsx
+++ src/components/WeatherCard/WeatherCard.jsx
@@ -1,17 +1,16 @@
 import "./WeatherCard.css";
 function WeatherCard(props) {
-  return props.weatherData.temp === -100 ? (
+  return props.weatherData.temp === 999 ? (
     <></>
   ) : (
-    <>
-      <section className="weather">
-        <p className="weather__temperature">{props.weatherData.temp}&deg; F</p>
-        <img
-          className="weather__image"
-          src={props.weatherData.conditionImage}
-        />
-      </section>
-    </>
+    <section className="weather">
+      <p className="weather__temperature">{props.weatherData.temp}&deg; F</p>
+      <img
+        className="weather__image"
+        alt={`Weather: ${props.weatherData.type}`}
+        src={props.weatherData.conditionImage}
+      />
+    </section>
   );
 }
 
