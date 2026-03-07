b59cb82baf54d10f69ce8ef60d5f33be3142456f
Joe Potenza <joseph.potenza@gmail.com>
2026-03-06 20:45:58 -0500

    Remove unnecessary empty tag

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -26,47 +26,42 @@ function Header({
   }
 
   return (
-    <>
-      <header
-        className={`header ${isProfilePage ? "header_page_profile" : ""} ${isMobileMenuOpened ? "header_menu_open" : ""}`}
-      >
-        <Link to="/" className="header__logo">
-          <img src={logo} alt="WTWR Logo" />
-        </Link>
-        <p className="header__location">
-          {currentDate}, {weatherData.city}
-        </p>
-        <div className="header__menu">
-          <img
-            src={closeMenuBtn}
-            alt="Close Menu"
-            className="header__menu-close-btn"
-            onClick={toggleMobileMenu}
-          />
-          <ToggleSwitch />
-          <button
-            className="header__add-clothes-btn"
-            onClick={openModalHandler}
-          >
-            + Add Clothes
-          </button>
-          <div className="header__user-container">
-            <Link to="/profile" className="header__user-name">
-              Terrence Tegegne
-            </Link>
-            <Link to="/profile" className="header__avatar">
-              <img src={avatar} alt="Joe Potenza" />
-            </Link>
-          </div>
-        </div>
+    <header
+      className={`header ${isProfilePage ? "header_page_profile" : ""} ${isMobileMenuOpened ? "header_menu_open" : ""}`}
+    >
+      <Link to="/" className="header__logo">
+        <img src={logo} alt="WTWR Logo" />
+      </Link>
+      <p className="header__location">
+        {currentDate}, {weatherData.city}
+      </p>
+      <div className="header__menu">
         <img
-          src={menuBtn}
-          alt="Menu"
-          className="header__menu-btn"
+          src={closeMenuBtn}
+          alt="Close Menu"
+          className="header__menu-close-btn"
           onClick={toggleMobileMenu}
         />
-      </header>
-    </>
+        <ToggleSwitch />
+        <button className="header__add-clothes-btn" onClick={openModalHandler}>
+          + Add Clothes
+        </button>
+        <div className="header__user-container">
+          <Link to="/profile" className="header__user-name">
+            Terrence Tegegne
+          </Link>
+          <Link to="/profile" className="header__avatar">
+            <img src={avatar} alt="Joe Potenza" />
+          </Link>
+        </div>
+      </div>
+      <img
+        src={menuBtn}
+        alt="Menu"
+        className="header__menu-btn"
+        onClick={toggleMobileMenu}
+      />
+    </header>
   );
 }
 
