5921bde75d3515a0bb6806ec79831f448583eae6
Joe Potenza <joseph.potenza@gmail.com>
2026-03-25 01:58:38 -0400

    Update auth.js

--- src/utils/auth.js
+++ src/utils/auth.js
@@ -10,18 +10,23 @@ export default class AuthApi {
 
   // single internal function for making API calls
   _makeAPICall({ endpoint, method = "GET", body = "", requireToken = false }) {
+    const headers = { ...this._headers };
+
+    // Add token if present and required
+    if (this._token && requireToken) {
+      headers.authorization = `Bearer ${this._token}`;
+    }
+
     const params = {
       method: method,
-      headers: this._headers,
+      headers: headers,
     };
+
     // Add body parameter when updating or adding content
     if (method === "PATCH" || method === "POST") {
       params.body = body;
     }
-    // Add token if present and required
-    if (this._token !== "" && requireToken) {
-      params.headers.authorization = `Bearer ${this._token}`;
-    }
+
     return fetch(`${this._baseUrl}${endpoint}`, params).then(checkResponse);
   }
 
